26969d3ad2509de18796b49cf69e7606136a1685
BlackAsLight <44320105+BlackAsLight@users.noreply.github.com>
2023-12-23 18:44:21 +1100

    Fixed Links

--- static/ts/main.ts
+++ static/ts/main.ts
@@ -1,5 +1,3 @@
-document.querySelectorAll('a[href^="/"]').forEach(aTag => aTag.setAttribute('href', '/The-Herald') + aTag.getAttribute('href')!)
-
 document.querySelectorAll<HTMLAnchorElement>('ol[data-link="index"] a').forEach((aTag, i) => {
 	aTag.setAttribute('href', '#index' + i)
 	document.querySelectorAll<HTMLHeadingElement>('h2[data-link="index"]')[ i ].setAttribute('id', 'index' + i)
